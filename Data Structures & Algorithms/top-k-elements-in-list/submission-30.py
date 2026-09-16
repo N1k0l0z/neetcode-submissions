1f8fca6f132b336504efe52c264fc9406ecaa5ee
N1k0l0z <kordzakhianikoloz01@gmail.com>
2026-09-16 13:27:07 -0700

    Add: top-k-elements-in-list - submission-30

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-30.py
@@ -0,0 +1,21 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        nums.sort()
+
+        hashing = {}
+        for i in range(0, len(nums)):
+            if nums[i] in hashing and nums[i] == nums[i-1]:
+                hashing[nums[i]] += 1   
+            else:
+                hashing[nums[i]] = 1       
+        asc = sorted(hashing.items(), key=lambda item: item[1], reverse = True)[:k]
+        result = []
+        for i in asc:
+            result.append(i[0])
+        return result
+
+
+        
+
+        
